a63bcb383927683c6e0236baaba15dd252ccf79d
Ugochukwu Avoaja <karlthegoo@hotmail.com>
2024-08-16 21:34:05 +0100

    terraform test

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "role_arn" {
   value       = module.github_oidc_role.role_arn
 }
 
-# output "name" {
-#   description = "The name of the IAM role from the module."
-#   value       = module.github_oidc_role.name 
-# }
+output "name" {
+  description = "The name of the IAM role from the module."
+  value       = module.github_oidc_role.name 
+}

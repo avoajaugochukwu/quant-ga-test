22b99f2787aecf4cadcce0cad83b243a2bde1f67
Ugochukwu Avoaja <karlthegoo@hotmail.com>
2024-08-20 20:13:10 +0100

    Update terraform

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "role_arn" {
-  description = "The ARN of the IAM role from the module."
-  value       = module.github_oidc_role.role_arn
-}
+# output "role_arn" {
+#   description = "The ARN of the IAM role from the module."
+#   value       = module.github_oidc_role.role_arn
+# }
 
 # output "name" {
 #   description = "The ARN of the IAM role from the module."
--- s3buckets.tf
+++ s3buckets.tf
@@ -7,3 +7,7 @@ resource "aws_s3_bucket" "bucket" {
 output "bucket_name" {
   value = aws_s3_bucket.bucket.bucket
 }
+
+output "bucket_arn" {
+  value = aws_s3_bucket.bucket.arn
+}

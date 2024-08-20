7da5fccb8a5e41601c0db0234238f5f244bf9263
Ugochukwu Avoaja <karlthegoo@hotmail.com>
2024-08-20 20:25:04 +0100

    Update terraform

--- s3buckets.tf
+++ s3buckets.tf
@@ -8,6 +8,6 @@ output "bucket_name" {
   value = aws_s3_bucket.bucket.bucket
 }
 
-# output "bucket_arn" {
-#   value = aws_s3_bucket.bucket.arn
-# }
+output "bucket_arn" {
+  value = aws_s3_bucket.bucket.arn
+}

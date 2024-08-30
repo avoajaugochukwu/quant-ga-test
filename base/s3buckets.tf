2c2e51a411b3a31f75761bd20cddd85ec57548d3
Ugochukwu Avoaja <karlthegoo@hotmail.com>
2024-08-30 21:39:05 +0100

    ++updated

--- base/s3buckets.tf
+++ base/s3buckets.tf
@@ -1,6 +1,6 @@
 # Create a bucket with a name that includes the current timestamp
 locals {
-  bucket_count = 2  # Number of buckets you want to create
+  bucket_count = 3  # Number of buckets you want to create
 }
 
 resource "aws_s3_bucket" "bucket" {
